39558eecac04645d5167e1d5bfeadedf272fd22d
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-24 03:19:15 +0800

    feat: image builder

--- src/sanity/sanity.utils.ts
+++ src/sanity/sanity.utils.ts
@@ -1,6 +1,14 @@
+import { createClient } from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
+
 export const config = {
   projectId: 'kxwuzftt',
   dataset: 'production',
   apiVersion: '2023-04-24',
   useCdn: true,
 };
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) =>
+  createImageUrlBuilder(config).image(source);
