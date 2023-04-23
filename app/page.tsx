c3ca14aefc2690b6a3af505fc04ba0d9be2fae4c
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-24 03:31:54 +0800

    feat: home page initial, fetch data from backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import { getPosts } from '@sanity-utils';
+import { urlFor } from '@/src/sanity/sanity.utils';
+import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts();
+
+  console.log(posts[0]);
   return (
-    <main></main>
-  )
+    <main>
+      <h1>{posts[0]?.title}</h1>
+      <Image
+        src={urlFor(posts[0]?.mainImage).url()}
+        alt={``}
+        height={400}
+        width={400}
+      />
+    </main>
+  );
 }
