aa3f7842b0aed7bc11e74fb9c4475486e34d1d9a
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-27 08:50:06 +0800

    fix: social media icon z-index

--- src/components/SocialMediaLinks/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks/SocialMediaLinks.tsx
@@ -4,7 +4,7 @@ import { SocialMediaItems } from './SocialMediaLinks.utils';
 
 export default function SocialMediaLinks() {
   return (
-    <div className='fixed right-[5px] top-[40%] z-100'>
+    <div className='fixed right-[5px] top-[40%] z-20'>
       <div className='flex flex-col space-y-3 px-2'>
         {SocialMediaItems?.map((social) => (
           <SocialIcon
