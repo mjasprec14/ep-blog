0a0b512f2d60a07baccbb919305cbcf9064e67da
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-29 02:50:28 +0800

    feat: Feature article component

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -11,22 +11,23 @@ export default function Featured({ posts }: Props) {
   const featuredPosts = posts.filter((post) => post.isFeaturedPost);
 
   const [latestPost] = [...featuredPosts];
-  console.log(latestPost);
+
   return (
     <section id='featured-article'>
-      <div className='max-w-7xl mt-5 mx-auto pb-4 md:px-10  text-white'>
-        <div className='h-[60vh] drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
+      <div className='max-w-7xl mt-5 mx-auto pb-4 group cursor-pointer text-white md:px-10'>
+        <div className='relative w-full h-[60vh] drop-shadow-xl group-hover:-translate-y-2 transition ease-in duration-150'>
           <Image
             src={urlFor(latestPost?.mainImage).url()}
             alt={`${latestPost.title} article photo `}
             fill
-            className='object-contain object-center'
+            className='object-cover object-center'
           />
           <BlogContent
             createdAt={latestPost?._createdAt}
             body={latestPost?.body}
             title={latestPost?.title}
             categories={latestPost?.categories}
+            route={`/post/${latestPost.slug.current}`}
           />
         </div>
       </div>
