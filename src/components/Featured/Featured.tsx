3fc6ca67d30ede85d900363b45acd874b49d5b73
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-28 23:55:43 +0800

    feat: Featured article initial

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -1,9 +1,35 @@
+import { urlFor } from '@/src/sanity/sanity.utils';
 import { Post } from '@sanity-typings';
+import Image from 'next/image';
+import BlogContent from '../BlogList/BlogContent';
 
 type Props = {
   posts: Post[];
 };
 
 export default function Featured({ posts }: Props) {
-  return <section id='featured-article'></section>;
+  const featuredPosts = posts.filter((post) => post.isFeaturedPost);
+
+  const [latestPost] = [...featuredPosts];
+  console.log(latestPost);
+  return (
+    <section id='featured-article'>
+      <div className='max-w-7xl mt-5 mx-auto pb-4 md:px-10  text-white'>
+        <div className='h-[60vh] drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out'>
+          <Image
+            src={urlFor(latestPost?.mainImage).url()}
+            alt={`${latestPost.title} article photo `}
+            fill
+            className='object-contain object-center'
+          />
+          <BlogContent
+            createdAt={latestPost?._createdAt}
+            body={latestPost?.body}
+            title={latestPost?.title}
+            categories={latestPost?.categories}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
