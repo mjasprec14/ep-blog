bf0b5e4421850d6b6a5634391f55cd818fdd9947
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-29 02:49:01 +0800

    fix: included routing and body text

--- src/components/BlogList/BlogContent.tsx
+++ src/components/BlogList/BlogContent.tsx
@@ -1,12 +1,15 @@
 import { Category } from '@sanity-typings';
-import { Block } from '@/src/sanity/typings/typings';
 import { PortableTextBlock } from 'sanity';
+import { PortableText } from '@portabletext/react';
+import { TbExternalLink } from 'react-icons/tb';
+import { ClientSideRoute } from '@components';
 
 type Props = {
   title: string;
   body: PortableTextBlock[];
   createdAt: string;
   categories: Category[];
+  route: string;
 };
 
 export default function BlogContent({
@@ -14,31 +17,57 @@ export default function BlogContent({
   title,
   createdAt,
   categories,
+  route,
 }: Props) {
   return (
-    <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between items-center'>
-      <div>
-        <p className='font-bold'>{title}</p>
+    <>
+      <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-3 flex justify-between items-center'>
+        <div className='space-y-2'>
+          <div className='flex flex-row justify-between items-center'>
+            <h3 className='font-bold'>{title}</h3>
 
-        <p>
-          {new Date(createdAt).toLocaleDateString('en-PH', {
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-          })}
-        </p>
-      </div>
-
-      <div className='flex flex-col gap-y-2 md:flex-row md:gap-x-2'>
-        {categories?.map((category) => (
-          <div
-            key={category?._id}
-            className='bg-white bg-opacity-20 text-black px-3 py-1 rounded-full text-xs md:text-sm font-semibold'
-          >
-            <p>{category?.title}</p>
+            <p className='text-sm'>
+              {new Date(createdAt).toLocaleDateString('en-PH', {
+                day: 'numeric',
+                month: 'long',
+                year: 'numeric',
+              })}
+            </p>
           </div>
-        ))}
+
+          <div className='line-clamp-2 text-sm'>
+            {body?.map((item, idx) => (
+              <PortableText
+                key={idx}
+                value={item}
+              />
+            ))}
+          </div>
+
+          <div className='flex flex-row gap-y-2 md:flex-row md:gap-x-2'>
+            {categories?.map((category) => (
+              <p
+                key={category?._id}
+                className='bg-blue-500 bg-opacity-40 text-white px-2 py-1 rounded-full text-xs md:text-xs font-semibold'
+              >
+                <span>{category?.title}</span>
+              </p>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className='absolute bottom-3 right-3'>
+        <ClientSideRoute route={route}>
+          <p className='hidden text-xs uppercase text-white font-semibold items-center group-hover:flex '>
+            Read Article
+            <TbExternalLink
+              className='ml-2'
+              size={18}
+            />
+          </p>
+        </ClientSideRoute>
+      </div>
+    </>
   );
 }
