e01dc5a048c8b77f44b211507249f84db6f42637
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-02 12:16:26 +0800

    feat: pageinfo schema

---
+++ src/sanity/schemas/pageInfo.ts
@@ -0,0 +1,61 @@
+const pageInfo = {
+  name: 'pageInfo',
+  title: 'Page Info',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'role',
+      title: 'Role',
+      type: 'string',
+    },
+    {
+      name: 'backgroundInformation',
+      title: 'BackgroundInformation',
+      type: 'string',
+    },
+    {
+      name: 'phoneNumber',
+      title: 'PhoneNumber',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    },
+    {
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+    },
+    {
+      name: 'socials',
+      title: 'Socials',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'social' } }],
+    },
+    {
+      name: 'heroImage',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'profilePic',
+      title: 'ProfilePic',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
+
+export default pageInfo;
