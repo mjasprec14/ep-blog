1a69c10c888c1527ad402f22aa89adbd6f708e65
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-02 11:58:23 +0800

    fix: social media position

--- src/components/SocialMediaLinks/SocialMediaLinks.tsx
+++ src/components/SocialMediaLinks/SocialMediaLinks.tsx
@@ -4,8 +4,8 @@ import { SocialMediaItems } from './SocialMediaLinks.utils';
 
 export default function SocialMediaLinks() {
   return (
-    <div className='fixed right-[5px] top-[40%] z-20'>
-      <div className='flex flex-col space-y-3 px-2'>
+    <div className='fixed left-1/2 -translate-x-1/2 bottom-0 z-20 border-t border-r rounded-tl-3xl rounded-tr-3xl px-3 pt-2 pb-1 bg-white backdrop-blur-lg bg-opacity-10'>
+      <div className='flex flex-row space-x-3 px-2'>
         {SocialMediaItems?.map((social) => (
           <SocialIcon
             key={social?.label}
