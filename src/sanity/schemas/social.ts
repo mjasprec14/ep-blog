89995195f85de7af10780cfa709de4698b6abdbc
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-02 12:16:40 +0800

    feat: social schema

---
+++ src/sanity/schemas/social.ts
@@ -0,0 +1,20 @@
+const social = {
+  name: 'social',
+  title: 'Social',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      description: 'Social media platforms',
+    },
+    {
+      name: 'url',
+      title: 'Url',
+      type: 'string',
+    },
+  ],
+};
+
+export default social;
