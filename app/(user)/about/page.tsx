d8017da0803365cbfbdc8c53a6590f0d9231393c
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-02 13:12:08 +0800

    feat: About page initial

--- app/(user)/about/page.tsx
+++ app/(user)/about/page.tsx
@@ -1,7 +1,21 @@
-export default function About() {
+import { getPageInfo } from '@sanity-utils';
+import { PageInfo } from '@sanity-typings';
+import Image from 'next/image';
+import { urlFor } from '@/src/sanity/sanity.utils';
+
+export default async function About() {
+  const pageInfo: PageInfo = await getPageInfo();
+
+  console.log(pageInfo);
   return (
-    <section>
-      <h1>About Page</h1>
+    <section className='w-full min-h-screen border border-red-500 text-red-500'>
+      <h1>ABOUT PAGE</h1>
+      <article>
+        <div className='w-1/2'>
+          {pageInfo.name}
+          {pageInfo.backgroundInformation}
+        </div>
+      </article>
     </section>
   );
 }
