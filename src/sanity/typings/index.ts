3a2c970d7bbb2b41ba7084734642cffc96566761
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-02 13:11:54 +0800

    feat: export PageInfo type

--- src/sanity/typings/index.ts
+++ src/sanity/typings/index.ts
@@ -1,3 +1,4 @@
 export type { default as Author } from './Author';
 export type { default as Category } from './Category';
 export type { default as Post } from './Post';
+export type { default as PageInfo } from './PageInfo';
