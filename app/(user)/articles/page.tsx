129b6cccab9742e733e534cc344f7700f7786ad7
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-03 15:40:19 +0800

    feat: initial design for articles page

--- app/(user)/articles/page.tsx
+++ app/(user)/articles/page.tsx
@@ -1,7 +1,27 @@
-export default function Articles() {
+import { getPosts } from '@sanity-utils';
+import { Post } from '@sanity-typings';
+
+import { ArticlesCard } from '@components';
+
+export default async function Articles() {
+  const posts: Post[] = await getPosts();
+
+  // console.log(posts);
   return (
-    <section>
-      <h1>Articles Page</h1>
+    <section className='min-h-screen w-full flex flex-col items-center justify-center py-16 space-y-8'>
+      <h1 className='text-xl text-left uppercase font-semibold text-gray-500 tracking-[20px]'>
+        Articles
+      </h1>
+      <div className='max-w-7xl h-screen'>
+        <div className='grid md:grid-cols-3 gap-3'>
+          {posts?.map((post) => (
+            <ArticlesCard
+              key={post?._id}
+              post={post}
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
