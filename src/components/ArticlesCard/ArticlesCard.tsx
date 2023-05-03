2a72c54367e96c60acf6eebe7472b779f35dce4f
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-03 18:40:21 +0800

    feat: link to article via client side routing

--- src/components/ArticlesCard/ArticlesCard.tsx
+++ src/components/ArticlesCard/ArticlesCard.tsx
@@ -2,6 +2,7 @@ import { urlFor } from '@/src/sanity/sanity.utils';
 import { Post } from '@sanity-typings';
 import Image from 'next/image';
 import { GoLinkExternal } from 'react-icons/go';
+import { ClientSideRoute } from '@components';
 
 type Props = {
   post: Post;
@@ -10,7 +11,7 @@ type Props = {
 export default function ArticlesCard({ post }: Props) {
   //   console.log(post);
   return (
-    <div className='relative h-[15rem] w-[21rem] md:w-[22rem] lg:w-[25rem] shadow-xl rounded-lg cursor-pointer group overflow-hidden'>
+    <div className='relative h-[15rem] w-[21rem] md:w-[22rem] lg:w-[25rem] shadow-xl rounded-lg group overflow-hidden'>
       <Image
         src={urlFor(post?.mainImage).url()}
         alt={`${post?.title} - article photo`}
@@ -19,10 +20,14 @@ export default function ArticlesCard({ post }: Props) {
       />
 
       <div className='opacity-0 absolute flex text-white bg-black/50 h-full w-full group-hover:opacity-100 transition-opacity duration-300 ease-in'>
-        <button className='m-auto border  rounded-full py-2 px-4 flex flex-row items-center space-x-3'>
-          <span>Read article</span>
-          <GoLinkExternal />
-        </button>
+        <div className='m-auto '>
+          <ClientSideRoute route={`/post/${post?.slug.current}`}>
+            <button className='border rounded-full py-2 px-4 flex flex-row items-center space-x-3'>
+              <span>Read article</span>
+              <GoLinkExternal />
+            </button>
+          </ClientSideRoute>
+        </div>
       </div>
 
       <div className='absolute bottom-0 left-0 bg-black/40 w-full py-3 text-center group-hover:translate-y-[60px] transition-transform duration-300 ease-out'>
