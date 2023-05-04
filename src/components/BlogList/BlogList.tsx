c5c4a0960a22c341150adfaf8047ec7cd45a283e
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-04 20:10:47 +0800

    feat: article headers and minor fixes

--- src/components/BlogList/BlogList.tsx
+++ src/components/BlogList/BlogList.tsx
@@ -13,29 +13,34 @@ export default function BlogList({ posts }: Props) {
 
   return (
     <section id='blog-list'>
-      <div className='max-w-7xl mx-auto px-2 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xs:px-10 gap-3 pb-24'>
-        {recentPost?.map((post) => (
-          <div
-            key={post?._id}
-            className='group cursor-pointer flex flex-col rounded-lg overflow-hidden'
-          >
-            <div className='relative w-full h-80 drop-shadow-xl'>
-              <Image
-                className='object-cover object-center rounded-lg'
-                src={urlFor(post?.mainImage).url()}
-                alt={`Article image - ${post?.title}`}
-                fill
-              />
-              <BlogContent
-                createdAt={post?._createdAt}
-                body={post?.body}
-                title={post?.title}
-                categories={post?.categories}
-                route={`/post/${post.slug.current}`}
-              />
+      <div className='max-w-7xl mx-auto px-2 xs:px-10'>
+        <h2 className='text-gray-500 text-xs text-center uppercase tracking-[12px] xs:text-left  xs:tracking-[20px] pb-4'>
+          Recent articles
+        </h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 pb-24'>
+          {recentPost?.map((post) => (
+            <div
+              key={post?._id}
+              className='group cursor-pointer flex flex-col rounded-lg overflow-hidden'
+            >
+              <div className='relative w-full h-80 drop-shadow-xl'>
+                <Image
+                  className='object-cover object-center rounded-lg'
+                  src={urlFor(post?.mainImage).url()}
+                  alt={`Article image - ${post?.title}`}
+                  fill
+                />
+                <BlogContent
+                  createdAt={post?._createdAt}
+                  body={post?.body}
+                  title={post?.title}
+                  categories={post?.categories}
+                  route={`/post/${post.slug.current}`}
+                />
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </section>
   );
--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -14,8 +14,11 @@ export default function Featured({ posts }: Props) {
 
   return (
     <section id='featured-article'>
-      <div className='max-w-7xl mx-auto mt-1 pb-4 px-2 xs:px-0 group cursor-pointer text-white md:px-10'>
-        <div className='relative w-full h-[60vh] drop-shadow-xl overflow-hidden'>
+      <div className='max-w-7xl mx-auto mt-1 pb-4 px-2 xs:px-2 group cursor-pointer  md:px-10'>
+        <h1 className='text-gray-500 text-xs text-center uppercase tracking-[12px] xs:text-left xs:tracking-[20px pt-8 pb-4'>
+          Featured article
+        </h1>
+        <div className='relative w-full h-[60vh] drop-shadow-xl text-white overflow-hidden'>
           <Image
             src={urlFor(latestPost?.mainImage).url()}
             alt={`${latestPost.title} article photo `}
