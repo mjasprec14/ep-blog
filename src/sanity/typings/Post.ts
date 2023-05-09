1eef671a75abbc0ab875d14ccd1fe61234de304d
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-09 18:12:41 +0800

    fix: remove commented code

--- src/sanity/typings/Post.ts
+++ src/sanity/typings/Post.ts
@@ -1,7 +1,6 @@
 import Author from './Author';
 import Category from './Category';
 import { Base, Block, Image, Slug } from './typings';
-// import { PortableTextBlock } from 'sanity';
 
 export default interface Post extends Base {
   author: Author;
