13f307472869ec133ee4ff6ddfed5b9f67a72f3c
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-09 18:13:39 +0800

    feat: Tweet Component init

---
+++ src/components/Tweet/Tweet.tsx
@@ -0,0 +1,11 @@
+export default function Tweet() {
+  return (
+    <section>
+      <div>
+        <div className='flex-1 break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter md:w-[360px] w-full h-fit'>
+          Tweet
+        </div>
+      </div>
+    </section>
+  );
+}
