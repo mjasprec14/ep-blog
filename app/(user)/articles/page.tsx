c5912c407d41390eaab1b91aa9238429a042c35d
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-09 15:00:24 +0800

    feat: Show more/less button for articles page

--- app/(user)/articles/page.tsx
+++ app/(user)/articles/page.tsx
@@ -1,25 +1,54 @@
+'use client';
 import { getPosts } from '@sanity-utils';
 import { Post } from '@sanity-typings';
+import { useState, useEffect } from 'react';
 
 import { ArticlesCard } from '@components';
 
-export default async function Articles() {
-  const posts: Post[] = await getPosts();
+export default function Articles() {
+  const [isShowMore, setShowMore] = useState<boolean>(false);
+  const [articles, setArticles] = useState<Post[]>([]);
 
-  // console.log(posts);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts: Post[] = await getPosts();
+      setArticles(posts);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className='min-h-screen w-full flex flex-col items-center justify-center py-16 space-y-8'>
       <h1 className='text-xl text-left uppercase font-semibold text-gray-500 tracking-[20px]'>
         Articles
       </h1>
-      <div className='max-w-7xl min-h-screen'>
+      <div className='max-w-7xl min-h-screen space-y-8'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-          {posts?.map((post) => (
-            <ArticlesCard
-              key={post?._id}
-              post={post}
-            />
-          ))}
+          {isShowMore
+            ? articles?.map((post) => (
+                <ArticlesCard
+                  key={post?._id}
+                  post={post}
+                />
+              ))
+            : articles.slice(0, 9)?.map((post) => (
+                <ArticlesCard
+                  key={post?._id}
+                  post={post}
+                />
+              ))}
+        </div>
+
+        <div className='grid'>
+          <button
+            onClick={() => {
+              setShowMore((prev) => !prev);
+            }}
+            className='m-auto border py-2 px-3 rounded-lg hover:bg-black hover:text-white'
+          >
+            {!isShowMore ? 'Show more' : 'Show less'}
+          </button>
         </div>
       </div>
     </section>
