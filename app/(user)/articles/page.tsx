c57824b124f8e3aea24ff3b8a5e3affc25963a43
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-11 22:00:22 +0800

    feat: Filter Search based on author, title or category

--- app/(user)/articles/page.tsx
+++ app/(user)/articles/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 import { getPosts } from '@sanity-utils';
 import { Post } from '@sanity-typings';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 
 import { ArticlesCard, SearchFilter } from '@components';
 
 export default function Articles() {
   const [isShowMore, setShowMore] = useState<boolean>(false);
   const [articles, setArticles] = useState<Post[]>([]);
+
+  const [searchText, setSearchText] = useState<string>('');
+  const [searchedResult, setSearchedResult] = useState<Post[]>([]);
+  const [searchTimeOut, setSearchTimeOut] = useState<any>(0);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -18,6 +22,33 @@ export default function Articles() {
     fetchPosts();
   }, []);
 
+  const filterArticles = (text: string): Post[] => {
+    const regex = new RegExp(text, 'i');
+
+    return articles.filter(
+      (article) =>
+        regex.test(article.title) ||
+        regex.test(article.author.name) ||
+        regex.test(article.categories[0].title)
+    );
+  };
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    clearTimeout(searchTimeOut);
+    setSearchText(e.target.value);
+
+    setSearchTimeOut(
+      setTimeout(() => {
+        setSearchedResult(filterArticles(e.target.value));
+      }, 500)
+    );
+  };
+
+  const handleCategoryClick = (category: string): void => {
+    setSearchText(category);
+    setSearchedResult(filterArticles(category));
+  };
+
   return (
     <section className='min-h-screen max-w-7xl mx-auto flex py-16'>
       <div className='mx-auto flex flex-col space-y-6'>
@@ -25,24 +56,28 @@ export default function Articles() {
           <h1 className='text-gray-500 text-xs text-center md:text-left uppercase tracking-[12px] xs:text-left xs:tracking-[20px] drop-shadow-lg'>
             Recent articles
           </h1>
-          <SearchFilter />
+          <SearchFilter handleSearchChange={handleSearchChange} />
         </div>
 
         <div className='min-h-screen space-y-8 '>
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3'>
-            {isShowMore
-              ? articles?.map((post) => (
-                  <ArticlesCard
-                    key={post?._id}
-                    post={post}
-                  />
-                ))
-              : articles.slice(0, 9)?.map((post) => (
-                  <ArticlesCard
-                    key={post?._id}
-                    post={post}
-                  />
-                ))}
+            {searchText
+              ? searchedResult
+                  .slice(0, isShowMore ? searchedResult.length : 9)
+                  .map((result) => (
+                    <ArticlesCard
+                      key={result?._id}
+                      post={result}
+                    />
+                  ))
+              : articles
+                  .slice(0, isShowMore ? articles.length : 9)
+                  .map((result) => (
+                    <ArticlesCard
+                      key={result?._id}
+                      post={result}
+                    />
+                  ))}
           </div>
 
           <div className='grid'>
