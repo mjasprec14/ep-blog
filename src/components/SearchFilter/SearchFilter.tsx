b6263c411709c89e2bd0651697eb2c85e3ab212d
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-11 21:59:52 +0800

    feat: handleChange prop

--- src/components/SearchFilter/SearchFilter.tsx
+++ src/components/SearchFilter/SearchFilter.tsx
@@ -1,10 +1,17 @@
-export default function SearchFilter() {
+import { ChangeEvent } from 'react';
+
+type Props = {
+  handleSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function SearchFilter({ handleSearchChange }: Props) {
   return (
     <div className='w-full md:w-auto'>
       <input
         type='text'
         placeholder='search...'
         className='block w-full rounded-md border border-gray-200 bg-white py-2.5 pl-5 pr-12 text-sm shadow-lg font-medium focus:border-black focus:outline-none focus:ring-0'
+        onChange={handleSearchChange}
       />
     </div>
   );
