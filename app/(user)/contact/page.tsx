9812b8d68077856a732142d2e9fad13201e6e49c
mjasprec <michaeljosephasprec1@gmail.com>
2023-05-12 20:39:01 +0800

    fix: ui styling adjustments

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -18,32 +18,38 @@ export default function Contact() {
   return (
     <section id='contact'>
       <div className='max-w-7xl mx-auto mt-1 pb-4 px-2 xs:px-2 md:px-10'>
-        <h1 className='text-gray-500 text-sm text-center uppercase tracking-[12px] xs:text-left xs:tracking-[20px pt-8 pb-4 drop-shadow-lg font-semibold'>
+        <h1 className='text-gray-500 text-sm text-center uppercase tracking-[12px] xs:tracking-[20px] pt-8 pb-4 drop-shadow-lg font-semibold'>
           Get in touch
         </h1>
 
         <div className=' min-h-screen w-full grid grid-cols-1 md:grid-cols-2 '>
           <div className='px-4'>
-            <h2 className='text-gray-500 text-xs text-center uppercase tracking-[12px] xs:text-left xs:tracking-[20px pt-8 pb-4 drop-shadow-lg font-semibold'>
+            <h2 className='text-gray-500 text-xs text-center uppercase tracking-[12px] pt-8 pb-4 drop-shadow-lg font-semibold'>
               Our Contact Info
             </h2>
             <div className='space-y-6 '>
               <div className='flex flex-row items-center space-x-6 flex-1 break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter min-w-[360px] w-full h-fit text-gray-500 dark:text-white'>
-                <SlPhone size={30} />
-                <span className='text-lg '>+639151321954</span>
+                <SlPhone size={25} />
+                <span className='text-sm font-semibold tracking-widest'>
+                  +639151321954
+                </span>
               </div>
               <div className='flex flex-row items-center space-x-6 flex-1 break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter min-w-[360px] w-full h-fit text-gray-500 dark:text-white'>
-                <TfiEmail size={30} />
-                <span className='text-lg '>me@gmail.com</span>
+                <TfiEmail size={25} />
+                <span className='text-sm font-semibold tracking-widest'>
+                  me@gmail.com
+                </span>
               </div>
               <div className='flex flex-row items-center space-x-6 flex-1 break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter min-w-[360px] w-full h-fit text-gray-500 dark:text-white'>
-                <SlLocationPin size={30} />
-                <span className='text-lg '>Philippines</span>
+                <SlLocationPin size={25} />
+                <span className='text-sm font-semibold tracking-widest'>
+                  Philippines
+                </span>
               </div>
             </div>
           </div>
           <div className=' px-4'>
-            <h2 className='text-gray-500 text-xs text-center uppercase tracking-[12px] xs:text-left xs:tracking-[20px pt-8 pb-4 drop-shadow-lg font-semibold'>
+            <h2 className='text-gray-500 text-xs text-center uppercase tracking-[12px] pt-8 pb-4 drop-shadow-lg font-semibold'>
               Send us a message
             </h2>
             <form className='w-full flex flex-col gap-7 rounded-xl border border-gray-200 bg-white/20 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur p-5 '>
